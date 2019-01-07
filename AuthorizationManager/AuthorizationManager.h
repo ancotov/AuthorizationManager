9ae7676d6a4e28ad33619f5d06a580ba5e490d4c
Котов Андрей <admin@mac-admin.local>
2019-01-07 18:28:32 +0300

    Initial Commit

---
+++ AuthorizationManager/AuthorizationManager.h
@@ -0,0 +1,19 @@
+//
+//  AuthorizationManager.h
+//  	
+//
+//  Created by Admin on 07.01.2019.
+//  Copyright © 2019 ancotov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AuthorizationManager.
+FOUNDATION_EXPORT double AuthorizationManagerVersionNumber;
+
+//! Project version string for AuthorizationManager.
+FOUNDATION_EXPORT const unsigned char AuthorizationManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AuthorizationManager/PublicHeader.h>
+
+
